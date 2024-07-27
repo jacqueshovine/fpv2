5f4a9359921cb1de49393144dd7f146c4c7e3066
Jacques Hovine <jacques.hovine@partoo.fr>
2024-07-27 14:22:51 +0200

    create films migration

---
+++ database/migrations/2024_07_27_122001_create_films_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('films', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->text('synopsis')->nullable();
+            $table->char('original_language', 2);
+            $table->integer('duration')->nullable(); // Duration in minutes
+            $table->float('rating', 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('films');
+    }
+};
