a15e977c09f16b41201fe70c8a713222891e0993
Jacques Hovine <jacques.hovine@partoo.fr>
2024-07-27 14:09:36 +0200

    create login view, routes and controller

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/login', 'App\Http\Controllers\AuthController@page')->name('login.page');
+Route::post('/login', 'App\Http\Controllers\AuthController@submit')->name('login.submit');
