9937de6540503558650123be713b5d6b5ee468b8
Jacques Hovine <jacques.hovine@partoo.fr>
2024-07-28 22:39:33 +0200

    create UserController and update routes

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,35 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
 })->name('home');
 
+/*
+|--------------------------------------------------------------------------
+| Login
+|--------------------------------------------------------------------------
+*/
+
 Route::get('/login', 'App\Http\Controllers\AuthController@login')->name('login.page');
 Route::post('/login', 'App\Http\Controllers\AuthController@submit')->name('login.submit');
+
+/*
+|--------------------------------------------------------------------------
+| User
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/users/{user}', [UserController::class, 'profile']);
+
+/*
+|--------------------------------------------------------------------------
+| // ! TESTING ROUTES
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/profile', function () {
+    return 'Test profile';
+})->middleware('auth.basic');
