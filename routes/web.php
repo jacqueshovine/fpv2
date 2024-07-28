21cae2f59c5359a545b198596ad63492c524cbb1
Jacques Hovine <jacques.hovine@partoo.fr>
2024-07-28 21:57:40 +0200

    rename home route and login action

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
 
-Route::get('/login', 'App\Http\Controllers\AuthController@page')->name('login.page');
+Route::get('/login', 'App\Http\Controllers\AuthController@login')->name('login.page');
 Route::post('/login', 'App\Http\Controllers\AuthController@submit')->name('login.submit');
